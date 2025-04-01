c76e494a3fce567015565b2e38163cb19a490f47
pedrobolson <pedbolson@gmail.com>
2025-04-01 15:49:16 -0300

    Creating the counter of together time

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import Carta from './components/Carta'
 import Footer from './components/Footer'
 import HeartBackground from './components/HeartBackground'
 import MusicPlayer from './components/MusicPlayer'
+import TogetherTimer from './components/TogetherTimer'
 
 function App() {
-  return(
+  return (
     <div className="app-container">
       <HeartBackground />
+      <TogetherTimer />
       <Carrossel />
       <Carta />
       <MusicPlayer />
